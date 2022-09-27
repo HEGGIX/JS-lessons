196cf392cf368b2c5870553675beb5098fcff29c
37sirotkin <80060465+37sirotkin@users.noreply.github.com>
2022-09-27 22:19:21 +0200

    Added sening messege feature

--- Messenger/script.js
+++ Messenger/script.js
@@ -1,5 +1,5 @@
 const data = {
-    contacts:[
+    contacts: [
         {
             name: "Alex"
         },
@@ -17,16 +17,47 @@ const leftPanelContacts = document.querySelector(".left-panel__contacts")
 function addContacts(data) {
     data.forEach(contact => {
         const divContact = document.createElement("div");
-    divContact.classList.add("left-panel__contacts__item")
+        divContact.classList.add("left-panel__contacts__item")
 
-    const contactName = document.createElement("div");
-    contactName.classList.add("contacts-name");
-    contactName.innerHTML = contact.name;
-    divContact.appendChild(contactName);
+        const contactName = document.createElement("div");
+        contactName.classList.add("contacts-name");
+        contactName.innerHTML = contact.name;
+        divContact.appendChild(contactName);
 
-    leftPanelContacts.appendChild(divContact); 
+        leftPanelContacts.appendChild(divContact);
     });
-   
+
 }
 
-addContacts(data.contacts)
+addContacts(data.contacts);
+
+
+const sendButton = document.getElementById('sendButton');
+const messegeInput = document.querySelector('.input-messege');
+const messegeContainer = document.querySelector('.right-panel__messege');
+
+sendButton.addEventListener('click', () => sendMessege());
+document.addEventListener('keydown', event => {
+    if (event.key === 'Enter') sendMessege();
+})
+
+function sendMessege() {
+    const messege = createDivMessege(messegeInput.value);
+    messegeContainer.appendChild(messege);
+
+    clearInput();
+}
+
+function createDivMessege(text) {
+    const wrap = document.createElement('div');
+    wrap.classList.add('wrap');
+    const messege = document.createElement('div');
+    messege.classList.add('messege');
+    messege.innerHTML = text;
+    wrap.appendChild(messege);
+    return wrap;    
+}
+
+function clearInput() {
+    messegeInput.value = '';
+}
